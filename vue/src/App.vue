02dcdd3157a3b0e84983381bf4d25776834ef886
Mike Lavan <mlavan737@gmail.com>
2021-08-02 13:04:30 -0400

    Navigation component and initial list items

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,9 +1,20 @@
 <template>
   <div id="app">
     <div id="nav">
+      <navigation/>
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
     </div>
     <router-view />
   </div>
 </template>
+
+<script>
+import Navigation from "./components/Navigation.vue";
+export default {
+  components: {
+    Navigation,
+  }
+  
+}
+</script>
---
+++ vue/src/components/Navigation.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="navigation">
+      <ul>
+          <li>Home</li>
+          <li>Login</li>
+          <li>Logout</li>
+          <li>Register</li>
+          <li>Create Pothole Report</li>
+      </ul>   
+  </div>   
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
