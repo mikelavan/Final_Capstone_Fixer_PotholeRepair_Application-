3a9184932f8aa069828a2cf649caee0e2cbd1535
David Bradley <dbradley978@gmail.com>
2021-08-02 17:25:01 -0400

    basic map working

--- vue/src/components/MapApi.vue
+++ vue/src/components/MapApi.vue
@@ -1,65 +1,131 @@
 <template>
-  <div id="map"></div>
+
+  <div id="map">
+    <h2>Search and add a pin</h2>
+      <GmapAutocomplete
+        @place_changed='setPlace'
+      />
+      <button
+        @click='addMarker'
+      >
+        Add
+      </button>
+      <br>
+    <GmapMap
+      :center='center'
+      :zoom='12'
+      style='width:100%;  height: 100vh;' @click="addMarker()"
+    >
+      <GmapMarker
+        :key="index"
+        v-for="(m, index) in markers"
+        :position="m.position"
+        @click="center=m.position"
+      />
+    </GmapMap>
+  </div>
+  
 </template>
-  <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
 <script
-      src="https://maps.googleapis.com/maps/api/js?key=%MAPS_API%&callback=initMap&libraries=&v=weekly&channel=2"
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDHkbrS_-WvALjcpa5jGAwtjDiYRVhH3Vg&callback=initMap&libraries=&v=weekly&channel=2"
       async
     ></script>
 <script>
 export default {
-name: 'map-api',
+  name: 'map-api',
+  data() {
+    return {
+      center: { lat: 45.508, lng: -73.587 },
+      currentPlace: null,
+      markers: [
+        {
+            position: {
+              lat: 40.7128, lng: 74.0060
+            }
+        },
+      ],
+      places: [],
+    }
+  },
+  mounted() {
+    this.geolocate();
+  },
+  methods: {
+    setPlace(place) {
+      this.currentPlace = place;
+    },
+    addMarker() {
+      if (this.currentPlace) {
+        const marker = {
+          lat: this.currentPlace.geometry.location.lat(),
+          lng: this.currentPlace.geometry.location.lng(),
+        };
+        this.markers.push({ position: marker });
+        this.places.push(this.currentPlace);
+        this.center = marker;
+        this.currentPlace = null;
+      }
+    },
+    geolocate: function() {
+      navigator.geolocation.getCurrentPosition(position => {
+        this.center = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+      });
+    },
+  },
 }
 // Note: This example requires that you consent to location sharing when
 // prompted by your browser. If you see the error "The Geolocation service
 // failed.", it means you probably did not give permission for the browser to
 // locate you.
-let map, infoWindow;
+// let map, infoWindow;
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 6,
-  });
-  infoWindow = new google.maps.InfoWindow();
-  const locationButton = document.createElement("button");
-  locationButton.textContent = "Pan to Current Location";
-  locationButton.classList.add("custom-map-control-button");
-  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
-  locationButton.addEventListener("click", () => {
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          };
-          infoWindow.setPosition(pos);
-          infoWindow.setContent("Location found.");
-          infoWindow.open(map);
-          map.setCenter(pos);
-        },
-        () => {
-          handleLocationError(true, infoWindow, map.getCenter());
-        }
-      );
-    } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false, infoWindow, map.getCenter());
-    }
-  });
-}
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById("map"), {
+//     center: { lat: -34.397, lng: 150.644 },
+//     zoom: 6,
+//   });
+//   infoWindow = new google.maps.InfoWindow();
+//   const locationButton = document.createElement("button");
+//   locationButton.textContent = "Pan to Current Location";
+//   locationButton.classList.add("custom-map-control-button");
+//   map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+//   locationButton.addEventListener("click", () => {
+//     // Try HTML5 geolocation.
+//     if (navigator.geolocation) {
+//       navigator.geolocation.getCurrentPosition(
+//         (position) => {
+//           const pos = {
+//             lat: position.coords.latitude,
+//             lng: position.coords.longitude,
+//           };
+//           infoWindow.setPosition(pos);
+//           infoWindow.setContent("Location found.");
+//           infoWindow.open(map);
+//           map.setCenter(pos);
+//         },
+//         () => {
+//           handleLocationError(true, infoWindow, map.getCenter());
+//         }
+//       );
+//     } else {
+//       // Browser doesn't support Geolocation
+//       handleLocationError(false, infoWindow, map.getCenter());
+//     }
+//   });
+// }
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(
-    browserHasGeolocation
-      ? "Error: The Geolocation service failed."
-      : "Error: Your browser doesn't support geolocation."
-  );
-  infoWindow.open(map);
-}
+// function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+//   infoWindow.setPosition(pos);
+//   infoWindow.setContent(
+//     browserHasGeolocation
+//       ? "Error: The Geolocation service failed."
+//       : "Error: Your browser doesn't support geolocation."
+//   );
+//   infoWindow.open(map);
+// }
 
 
 </script>
--- vue/src/main.js
+++ vue/src/main.js
@@ -3,8 +3,16 @@ import App from './App.vue'
 import router from './router/index'
 import store from './store/index'
 import axios from 'axios'
+import * as VueGoogleMaps from 'vue2-google-maps'
 
 Vue.config.productionTip = false
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyDHkbrS_-WvALjcpa5jGAwtjDiYRVhH3Vg',
+    libraries: 'places',
+  }
+});
 
 axios.defaults.baseURL = process.env.VUE_APP_REMOTE_API;
 
