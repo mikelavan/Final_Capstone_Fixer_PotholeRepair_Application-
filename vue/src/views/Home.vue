38f709b9c4700f2e0cd7d5499c93871e36ea28f9
Hunter Steffes <hrsteffes@gmail.com>
2021-08-02 16:15:47 -0400

    pushing MapApi components

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -2,11 +2,20 @@
   <div class="home">
     <h1>Home</h1>
     <p>You must be authenticated to see this</p>
+    <map-api />
   </div>
 </template>
 
+
 <script>
+import MapApi from '@/components/MapApi.vue'
+
+
 export default {
-  name: "home"
+  name: "home",
+  components : {
+    MapApi
+  }
+
 };
 </script>
