209d63da9e54bdccbbb18d1157fa8372d43104b3
David Bradley <dbradley978@gmail.com>
2021-08-03 09:28:04 -0400

    map edits

--- vue/src/components/MapApi.vue
+++ vue/src/components/MapApi.vue
@@ -1,163 +1,72 @@
 <template>
-
-  <div id="map">
-    <h2>Search and add a pin</h2>
-      <GmapAutocomplete
-        @place_changed='setPlace'
-      />
-      <button
-        @click='addMarker'
-      >
-        Add
-      </button>
-      <br>
-    <GmapMap
-      :center='center'
-      :zoom='12'
-      style='width:100%;  height: 100vh;' @click="addMarker()"
-    >
-      <GmapMarker
-        :key="index"
-        v-for="(m, index) in markers"
-        :position="m.position"
-        @click="center=m.position"
-      />
-    </GmapMap>
-  </div>
-  
+  <div id="map" ref="map">
+			<div id="markers" v-for="marker in markers" :key="marker.id">
+				<map-marker :lat="marker.latitude" :lng="marker.longitude"></map-marker>
+				<map-info-window :lat="marker.latitude + .2" :lng="marker.longitude">Test 1</map-info-window>
+			</div>
+			
+			<!-- <map-info-window :lat="-23.344" :lng="129.036">
+				<span style="background:red;">Test 2</span>
+			</map-info-window>
+			<map-info-window :lat="-22.344" :lng="128.036">
+				<img
+					src="https://1.bp.blogspot.com/_S2Wkv5XI8JI/TUL5MbRnT-I/AAAAAAAAACA/g8V_yjojceA/s1600/woot.jpg"
+					height="100px"
+				/>
+			</map-info-window> -->
+		</div>
 </template>
-<script
-      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDHkbrS_-WvALjcpa5jGAwtjDiYRVhH3Vg&callback=initMap&libraries=&v=weekly&channel=2"
-      async
-    ></script>
+
 <script>
-export default {
-  name: 'map-api',
-  data() {
-    return {
-      center: { lat: 45.508, lng: -73.587 },
-      currentPlace: null,
-      markers: [
-        {
-            position: {
-              lat: 40.7128, lng: 74.0060
-            }
-        },
-      ],
-      places: [],
-    }
-  },
-  mounted() {
-    this.geolocate();
-  },
-  methods: {
-    setPlace(place) {
-      this.currentPlace = place;
-    },
-    addMarker() {
-      if (this.currentPlace) {
-        const marker = {
-          lat: this.currentPlace.geometry.location.lat(),
-          lng: this.currentPlace.geometry.location.lng(),
-        };
-        this.markers.push({ position: marker });
-        this.places.push(this.currentPlace);
-        this.center = marker;
-        this.currentPlace = null;
-      }
-    },
-    geolocate: function() {
-      navigator.geolocation.getCurrentPosition(position => {
-        this.center = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-        };
-      });
-    },
-  },
-}
-// Note: This example requires that you consent to location sharing when
-// prompted by your browser. If you see the error "The Geolocation service
-// failed.", it means you probably did not give permission for the browser to
-// locate you.
-// let map, infoWindow;
+  import MapMarker from "@/components/MapMarker"
+	import MapInfoWindow from "@/components/MapInfoWindow"
 
-// function initMap() {
-//   map = new google.maps.Map(document.getElementById("map"), {
-//     center: { lat: -34.397, lng: 150.644 },
-//     zoom: 6,
-//   });
-//   infoWindow = new google.maps.InfoWindow();
-//   const locationButton = document.createElement("button");
-//   locationButton.textContent = "Pan to Current Location";
-//   locationButton.classList.add("custom-map-control-button");
-//   map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
-//   locationButton.addEventListener("click", () => {
-//     // Try HTML5 geolocation.
-//     if (navigator.geolocation) {
-//       navigator.geolocation.getCurrentPosition(
-//         (position) => {
-//           const pos = {
-//             lat: position.coords.latitude,
-//             lng: position.coords.longitude,
-//           };
-//           infoWindow.setPosition(pos);
-//           infoWindow.setContent("Location found.");
-//           infoWindow.open(map);
-//           map.setCenter(pos);
-//         },
-//         () => {
-//           handleLocationError(true, infoWindow, map.getCenter());
-//         }
-//       );
-//     } else {
-//       // Browser doesn't support Geolocation
-//       handleLocationError(false, infoWindow, map.getCenter());
-//     }
-//   });
-// }
-
-// function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-//   infoWindow.setPosition(pos);
-//   infoWindow.setContent(
-//     browserHasGeolocation
-//       ? "Error: The Geolocation service failed."
-//       : "Error: Your browser doesn't support geolocation."
-//   );
-//   infoWindow.open(map);
-// }
-
-
+	export default {
+		components: {
+			MapMarker,
+			MapInfoWindow
+		},
+		data: () => ({
+			map: null,
+			markers: [
+				{
+					latitude: -27.344,
+					longitude: 133.036
+				},
+				{
+					latitude: -26.344,
+					longitude: 132.036
+				},
+				{
+					latitude: -25.344,
+					longitude: 131.036
+				}
+			],
+		}),
+		methods: {
+			getMap(callback) {
+				let vm = this
+				function checkForMap() {
+					if (vm.map) callback(vm.map)
+					else setTimeout(checkForMap, 200)
+				}
+				checkForMap()
+			}
+		},
+		mounted() {
+			this.map = new window.google.maps.Map(this.$refs["map"], {
+				center: { lat: -23.444, lng: 129.036 },
+				zoom: 7
+			})
+		}
+	}
 </script>
 
 <style>
 /* Always set the map height explicitly to define the size of the div
        * element that contains the map. */
 #map {
-  height: 100%;
-}
-
-/* Optional: Makes the sample page fill the window. */
-html,
-body {
-  height: 100%;
-  margin: 0;
-  padding: 0;
-}
-
-.custom-map-control-button {
-  background-color: #fff;
-  border: 0;
-  border-radius: 2px;
-  box-shadow: 0 1px 4px -1px rgba(0, 0, 0, 0.3);
-  margin: 10px;
-  padding: 0 0.5em;
-  font: 400 18px Roboto, Arial, sans-serif;
-  overflow: hidden;
-  height: 40px;
-  cursor: pointer;
-}
-.custom-map-control-button:hover {
-  background: #ebebeb;
-}
+		height: 600px;
+		background: gray;
+	}
 </style>
--- vue/src/main.js
+++ vue/src/main.js
@@ -3,16 +3,16 @@ import App from './App.vue'
 import router from './router/index'
 import store from './store/index'
 import axios from 'axios'
-import * as VueGoogleMaps from 'vue2-google-maps'
-
-Vue.config.productionTip = false
+import * as VueGoogleMaps from "vue2-google-maps"
 
 Vue.use(VueGoogleMaps, {
   load: {
-    key: 'AIzaSyDHkbrS_-WvALjcpa5jGAwtjDiYRVhH3Vg',
-    libraries: 'places',
+    key: "AIzaSyDHkbrS_-WvALjcpa5jGAwtjDiYRVhH3Vg",
+    libraries: "places"
   }
 });
+
+Vue.config.productionTip = false
 
 axios.defaults.baseURL = process.env.VUE_APP_REMOTE_API;
 
