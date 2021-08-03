7cdde861c2f04e02c2bfcc8ef4301d7db2b45f37
Domenico Siravo <domsiravo@gmail.com>
2021-08-03 10:08:34 -0400

    Reverted services back to jdbcDAO

--- java/src/main/java/com/techelevator/services/PotholeInformationService.java
+++ java/src/main/java/com/techelevator/services/PotholeInformationService.java
@@ -1,22 +1,22 @@
-package com.techelevator.services;
-
-import com.techelevator.dao.JdbcPotholeInformation;
-import com.techelevator.model.PotholeInformation;
-import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-
-@Service
-public class PotholeInformationService {
-    private JdbcPotholeInformation potholeInformation;
-
-//    public PotholeInformationService() {}
-
-    public PotholeInformationService(JdbcPotholeInformation potholeInformation) {
-        this.potholeInformation = potholeInformation;
-    }
-
-    public ArrayList<PotholeInformation> list() {
-        return potholeInformation.getPotholes();
-    }
-}
+//package com.techelevator.services;
+//
+//import com.techelevator.dao.JdbcPotholeInformation;
+//import com.techelevator.model.PotholeInformation;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.ArrayList;
+//
+//@Service
+//public class PotholeInformationService {
+//    private JdbcPotholeInformation potholeInformation;
+//
+////    public PotholeInformationService() {}
+//
+//    public PotholeInformationService(JdbcPotholeInformation potholeInformation) {
+//        this.potholeInformation = potholeInformation;
+//    }
+//
+//    public ArrayList<PotholeInformation> list() {
+//        return potholeInformation.getPotholes();
+//    }
+//}
