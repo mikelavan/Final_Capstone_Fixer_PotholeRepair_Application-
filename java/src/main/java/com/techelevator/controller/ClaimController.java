9b756da005afeb0a57918f931d33d41f0911bd35
Hunter Steffes <hrsteffes@gmail.com>
2021-08-09 15:36:57 -0400

    rested controller

--- java/src/main/java/com/techelevator/controller/ClaimController.java
+++ java/src/main/java/com/techelevator/controller/ClaimController.java
@@ -3,15 +3,11 @@ package com.techelevator.controller;
 import com.techelevator.model.Claim;
 import com.techelevator.services.ClaimService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
-@CrossOrigin
-@Controller
+@RestController
 public class ClaimController {
 
     private ClaimService claimService;
@@ -20,7 +16,7 @@ public class ClaimController {
         this.claimService = claimService;
     }
 
-    @RequestMapping(path="/claim", method = RequestMethod.POST)
+    @RequestMapping(path ="/claim", method = RequestMethod.POST)
     public void create(@Valid @RequestBody Claim claim){
         claimService.makeClaim(claim);
     }
