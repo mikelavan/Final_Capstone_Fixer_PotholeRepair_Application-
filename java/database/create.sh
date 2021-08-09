7347c8da2f1273ec2ee7f9909c5318acba35b495
Hunter Steffes <hrsteffes@gmail.com>
2021-08-09 14:13:39 -0400

    updated database and create.sh script to include claims table

--- java/database/create.sh
+++ java/database/create.sh
@@ -6,5 +6,6 @@ createdb -U postgres $DATABASE &&
 psql -U postgres -d $DATABASE -f "$BASEDIR/schema.sql" &&
 psql -U postgres -d $DATABASE -f "$BASEDIR/potholeInfo.sql" &&
 psql -U postgres -d $DATABASE -f "$BASEDIR/schedule.sql" &&
+psql -U postgres -d $DATABASE -f "$BASEDIR/claims.sql" &&
 psql -U postgres -d $DATABASE -f "$BASEDIR/employeeRole.sql" &&
 psql -U postgres -d $DATABASE -f "$BASEDIR/user.sql"
