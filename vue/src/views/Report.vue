25195d3895a251256790739ac984717df0a8272f
David Bradley <dbradley978@gmail.com>
2021-08-10 17:03:05 -0400

    report vue

---
+++ vue/src/views/Report.vue
@@ -0,0 +1,61 @@
+<template>
+    <div id="report">
+        test
+        <input type="file" id="file"  ref="fileInput" />'
+        <button type="submit" v-on:click="submitPicture()">Submit</button>
+    </div>
+</template>
+
+<script>
+import PotholeService from '../services/PotholeService'
+export default {    
+    data: () => ({
+        file: this.$refs.fileInput.files[0],
+        potholeId: this.$route.params.id,
+    }),
+    methods: {
+        submitPicture() {
+            let formData  = new FormData();
+            formData.append('file', this.file);
+
+
+                const options = {
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    }
+                }
+
+
+
+                // console.log(schedule);
+
+        
+
+
+				PotholeService.submitPicture(formData, options, this.potholeId).then(response => {
+                    if (response.status === 200) {
+                        alert("Picture Submitted");
+                    }
+			}).catch(error => {
+				if(error.response) {
+					console.log('Error submitting new report.');
+				} else if (error.request) {
+					console.log("Error submitting new board. Server could not be reached.");
+				} else {
+					console.log("Error submitting new board. Request could not be created.");
+				}
+			})
+            }
+
+        },
+                
+
+}
+</script>
+
+<style>
+#report {
+    margin-top:  10vh;
+    padding: 5%;
+}
+</style>
